b7f04124afa95e9f9a269c71e6acb86034e468c8
ljt <liujiatai2016@163.com>
2019-05-27 11:59:06 +0800

    注释

--- src/main/java/com/design/patterns/designpatterns/bridge/Implementor.java
+++ src/main/java/com/design/patterns/designpatterns/bridge/Implementor.java
@@ -11,6 +11,14 @@ package com.design.patterns.designpatterns.bridge;
  *
  * 实现化角色
  */
+
+/**
+ * 桥接模式的应用场景
+ *     桥接模式通常适用于以下场景。
+ *     当一个类存在两个独立变化的维度，且这两个维度都需要进行扩展时。
+ *     当一个系统不希望使用继承或因为多层次继承导致系统类的个数急剧增加时。
+ *     当一个系统需要在构件的抽象化角色和具体化角色之间增加更多的灵活性时。
+ */
 public interface Implementor {
 
     void operationImpl();
--- src/test/java/com/design/patterns/designpatterns/DesignPatternsApplicationTests.java
+++ src/test/java/com/design/patterns/designpatterns/DesignPatternsApplicationTests.java
@@ -5,6 +5,9 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DesignPatternsApplicationTests {
@@ -13,4 +16,21 @@ public class DesignPatternsApplicationTests {
     public void contextLoads() {
     }
 
+
+    @Test
+    public void test_01() {
+        String[] result = new String[2];
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i + "";
+                    result[1] = j + "";
+                }
+            }
+        }
+        String s = Arrays.asList(result).stream().collect(Collectors.joining(","));
+        System.out.println(s);
+    }
 }
